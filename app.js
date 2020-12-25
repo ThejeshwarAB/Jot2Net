8793543c84d30a555941d9b3c1376f16c4687521
ThejeshwarAB <abibastheja@gmail.com>
2020-12-25 08:31:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require("path");
+
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+// const { getMaxListeners } = require('process');
+
+app.listen(3000,(err) =>{
+    if(err)
+    throw err;
+    else
+    console.log("Server is running")
+})
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: "nuser3899@gmail.com",
+        pass: "ebbmaxxrujjncknp"
+    },
+    tls: {
+        rejectUnauthorized: false,
+        secureProtocol: "TLSv1_method"
+    }
+});
+
+app.get('/',(req,res)=>{   
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.post('/',(req,res)=>{
+    var mailOptions = {
+        from: "nuser3899@gmail.com",
+        to: req.body.mail,
+        subject: req.body.subj + " from " + req.body.name, 
+        text: req.body.content,
+    };
+
+    transporter.sendMail(mailOptions,function(err,info){
+        if(err){
+            console.log(err)
+            res.send("Mail not sent. Try again!");}
+        else
+            res.send("Mail sent successfully");
+    })
+})
